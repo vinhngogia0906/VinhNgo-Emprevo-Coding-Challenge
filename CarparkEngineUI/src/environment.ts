e180dcdffa31af3e923dd5d02149b224ac1bfe38
Vinh Ngo (s3578687) <41614369+vinhngogia0906@users.noreply.github.com>
2024-08-11 16:53:36 +1000

    added Angular Frontend app

---
+++ CarparkEngineUI/src/environment.ts
@@ -0,0 +1,9 @@
+export const test_environment = {
+  production: false,
+  graphqlUri: 'https://localhost:7172/graphql'
+};
+
+export const prod_environment = {
+  production: true,
+  graphqlUri: 'https://localhost:7172/graphql'
+};
