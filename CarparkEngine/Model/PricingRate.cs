9c6be4317c7e11acc540cd32c2bed54e5fb8f584
Vinh Ngo (s3578687) <41614369+vinhngogia0906@users.noreply.github.com>
2024-08-11 15:39:55 +1000

    Added model classes

---
+++ CarparkEngine/Model/PricingRate.cs
@@ -0,0 +1,13 @@
+﻿namespace CarparkEngine.Model
+{
+    // PricingRate object, used in the response of get PricingRates query
+    public class PricingRate
+    {
+        public string? Name { get; set; }
+        public string? Type { get; set; }
+        public float TotalPrice { get; set; }
+        public string? EntryCondition { get; set; }
+        public string? ExitCondition { get; set; }
+        public string? Note { get; set; }
+    }
+}
