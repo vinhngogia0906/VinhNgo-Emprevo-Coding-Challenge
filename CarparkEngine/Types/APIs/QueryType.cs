11401a7d2f12f2190bb8a148318a9fec209a6456
Vinh Ngo (s3578687) <41614369+vinhngogia0906@users.noreply.github.com>
2024-08-11 15:50:00 +1000

    Added Type description for GraphQL documentation

---
+++ CarparkEngine/Types/APIs/QueryType.cs
@@ -0,0 +1,22 @@
+﻿using CarparkEngine.API;
+
+namespace CarparkEngine.Types.APIs
+{
+    // Schema description for Query APIs
+    public class QueryType : ObjectTypeExtension<Query>
+    {
+        protected override void Configure(IObjectTypeDescriptor<Query> descriptor)
+        {
+            descriptor.Name(OperationTypeNames.Query);
+
+            // Explain the Instruction Query
+            descriptor
+                .Field(i => i.Instruction)
+                .Description("Retrieves the welcome message and instruction how to get to the submit ticket api document.");
+            // Explain the PricingRates Query
+            descriptor
+                .Field(p => p.PricingRates())
+                .Description("Retrieves the pricing convention of Carpark Engine.");
+        }
+    }
+}
---
+++ CarparkEngine/Types/Requests/PatronTicketInputType.cs
@@ -0,0 +1,26 @@
+﻿using CarparkEngine.Model;
+
+namespace CarparkEngine.Types.Requests
+{
+    public class PatronTicketInputType : InputObjectType<PatronTicketInput>
+    {
+        // Schema description for PatronTicketInput Object
+        protected override void Configure(IInputObjectTypeDescriptor<PatronTicketInput> descriptor)
+        {
+            descriptor.Name("PatronTicketInput");
+            descriptor.Description("Represents the input to submit the ticket to get the price result");
+
+            // Description for entry time input
+            descriptor
+                .Field(t => t.entry)
+                .Description("Represents the entry date time input")
+                .Type<DateTimeType>();
+
+            // Description for exit time input
+            descriptor
+                .Field(t => t.exit)
+                .Description("Represents the exit date time input")
+                .Type<DateTimeType>();
+        }
+    }
+}
