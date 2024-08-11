a4c683e7c427cab979495264c96663e8a0f89e81
Vinh Ngo (s3578687) <41614369+vinhngogia0906@users.noreply.github.com>
2024-08-11 16:57:44 +1000

    added graphQL, submit and result component for the CarparkEngineUI app

---
+++ CarparkEngineUI/src/app/submit/submit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-submit',
+  standalone: true,
+  imports: [],
+  templateUrl: './submit.component.html',
+  styleUrl: './submit.component.css'
+})
+export class SubmitComponent {
+
+}
